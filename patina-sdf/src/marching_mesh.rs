2135cd3d8e4c884d56aea7e8f30136e7a808aa3d
Nathan Dobson <nadobson@mac.com>
2025-07-08 17:40:17 -0700

    Re-added interpolation.

--- patina-sdf/src/marching_mesh.rs
+++ patina-sdf/src/marching_mesh.rs
@@ -19,53 +19,6 @@ use patina_vec::vec3::Vec3;
 use patina_vec::vector3::Vector3;
 use std::collections::{HashMap, HashSet};
 use std::mem;
-// #[derive(Debug)]
-// struct OctreeCube {
-//     min: [usize; 3],
-//     dim: usize,
-// }
-//
-// impl OctreeCube {
-//     pub fn new(min: [usize; 3], dim: usize) -> OctreeCube {
-//         assert!(dim.is_power_of_two());
-//         OctreeCube { min, dim }
-//     }
-//     pub fn min(&self) -> [usize; 3] {
-//         self.min
-//     }
-//     pub fn max(&self) -> [usize; 3] {
-//         self.min.map(|x| x + self.dim)
-//     }
-//
-//     pub fn dim(&self) -> usize {
-//         self.dim
-//     }
-//     pub fn subcubes(&self) -> Option<([usize; 3], [Self; 8])> {
-//         if self.dim == 2 {
-//             return None;
-//         }
-//         let dim2 = self.dim / 2;
-//         let mut subcubes = ArrayVec::new();
-//         for x in 0..2 {
-//             for y in 0..2 {
-//                 for z in 0..2 {
-//                     subcubes.push(OctreeCube::new(
-//                         [
-//                             self.min[0] + x * dim2,
-//                             self.min[1] + y * dim2,
-//                             self.min[2] + z * dim2,
-//                         ],
-//                         dim2,
-//                     ));
-//                 }
-//             }
-//         }
-//         Some((
-//             [self.min[0] + dim2, self.min[1] + dim2, self.min[2] + dim2],
-//             subcubes.into_inner().ok().unwrap(),
-//         ))
-//     }
-// }
 
 #[derive(Debug)]
 struct MarchingNode {}
@@ -77,7 +30,6 @@ pub struct MarchingMesh {
     vertices: Vec<Vec3>,
     vertex_table: HashMap<Vector3<NotNan<f64>>, usize>,
     triangles: Vec<MeshTriangle>,
-    triangle_set: HashSet<[usize; 3]>,
 }
 
 impl Default for MarchingNode {
@@ -98,112 +50,8 @@ impl MarchingMesh {
             vertices: vec![],
             vertex_table: HashMap::new(),
             triangles: vec![],
-            triangle_set: HashSet::new(),
         }
     }
-    // fn position(&self, ints: [usize; 3]) -> Vec3 {
-    //     let dimensions = self.aabb.dimensions();
-    //     (0..3)
-    //         .map(|axis| {
-    //             (ints[axis] as f64) / (self.render_dim as f64) * dimensions[axis]
-    //                 + self.aabb.min()[axis]
-    //         })
-    //         .collect()
-    // }
-    // fn aabb(&self, cube: &OctreeCube) -> Aabb {
-    //     Aabb::new(self.position(cube.min()), self.position(cube.max()))
-    // }
-    // fn evaluate(&mut self, ints: [usize; 3], sdf: &Sdf) -> f64 {
-    //     let position = self.position(ints);
-    //     sdf.evaluate(position)
-    // }
-    // fn build_cube(&mut self, cube: OctreeCube, sdf: &Sdf) {
-    //     let aabb = self.aabb(&cube);
-    //     let aabb_intervals: Vector3<DecInterval> = (0..3)
-    //         .map(|axis| DecInterval::try_from((aabb.min()[axis], aabb.max()[axis])).unwrap())
-    //         .collect();
-    //     let (sdf2, range) = sdf.evaluate_constrain(aabb_intervals);
-    //     if !range.contains(0.0) {
-    //         return;
-    //     }
-    //     let sdf = sdf2.unwrap_or(sdf.clone());
-    //     if let Some((center, subcubes)) = cube.subcubes() {
-    //         let radius = self.evaluate(center, &sdf).abs();
-    //         let min_radius = aabb.dimensions().length() / 2.0 + 10e-5;
-    //         if radius <= min_radius {
-    //             for cube in subcubes {
-    //                 self.build_cube(cube, &sdf);
-    //             }
-    //         }
-    //     } else {
-    //         let mut bits = ArrayVec::new();
-    //         let [x, y, z] = cube.min();
-    //         for dx in 0..2 {
-    //             for dy in 0..2 {
-    //                 for dz in 0..2 {
-    //                     let ints = [x + dx * 2, y + dy * 2, z + dz * 2];
-    //                     let d = self.evaluate(ints, &sdf);
-    //                     bits.push(d >= 0.0);
-    //                 }
-    //             }
-    //         }
-    //         let mcube = marching_cube(bits.into_inner().unwrap());
-    //         let mut cube_vertices = vec![];
-    //         for vertex in mcube.vertices() {
-    //             cube_vertices.push(self.add_vertex(
-    //                 [
-    //                     x + vertex.x() as usize,
-    //                     y + vertex.y() as usize,
-    //                     z + vertex.z() as usize,
-    //                 ],
-    //                 &sdf,
-    //             ));
-    //         }
-    //         for triangle in mcube.triangles() {
-    //             self.triangles.push(MeshTriangle::from(
-    //                 triangle.vertices().map(|v| cube_vertices[v]),
-    //             ));
-    //         }
-    //     }
-    // }
-    // fn add_vertex(&mut self, ints: [usize; 3], sdf: &Sdf) -> usize {
-    //     let position = self.position(ints);
-    //     *self.vertex_table.entry(ints).or_insert_with(|| {
-    //         let position_deriv = |t| {
-    //             let mut inputs = Vector3::splat(Deriv::nan());
-    //             for axis in 0..3 {
-    //                 if ints[axis] % 2 == 0 {
-    //                     inputs[axis] = Deriv::constant(position[axis]);
-    //                 } else {
-    //                     inputs[axis] = Deriv::constant(position[axis])
-    //                         + Deriv::variable(t, 0)
-    //                             * Deriv::constant(
-    //                                 self.aabb.dimensions()[axis] / (self.render_dim as f64),
-    //                             );
-    //                 }
-    //             }
-    //             inputs
-    //         };
-    //         let lsdf = |t| sdf.evaluate_deriv1(position_deriv(t));
-    //         let t = Newton::new().solve(-1.0..1.0, lsdf);
-    //         let t = if let Some(t) = t {
-    //             t.into_inner()
-    //         } else {
-    //             println!("Cannot solve {:?}", ints);
-    //             0.5
-    //         };
-    //         assert!(t >= -1.0 && t <= 1.0, "{:?}", t);
-    //         let t = t.clamp(-0.99, 0.99);
-    //         let position = position_deriv(t).map(|x| x.value());
-    //         let index = self.vertices.len();
-    //         self.vertices.push(position);
-    //         index
-    //     })
-    // }
-    // pub fn build(mut self, sdf: &Sdf) -> Mesh {
-    //     self.build_cube(OctreeCube::new([0, 0, 0], self.render_dim), sdf);
-    //     Mesh::new(self.vertices, self.triangles)
-    // }
     fn find_marching_cube(&self, aabb: &Aabb, sdf: &Sdf) -> CubeTriMesh {
         let mut result = CubeVertexSet::new();
         for cv in cube_corners() {
@@ -272,23 +120,6 @@ impl MarchingMesh {
         for cv in cube_points() {
             if to_sample[cv] {
                 let v = self.path_position(octree.path(), cv);
-                // let numer = octree.path().position() * 2 + cv.map(|x| x as usize);
-                // let denom = (2 << octree.path().depth()) as f64;
-                // let v = numer.map(|x| x as f64) / denom;
-                // let v = self.aabb.min() + v.mul_elements(self.aabb.dimensions());
-                // let v: Vec3 = (0..3)
-                //     .map(|axis| {
-                //
-                //         let start = aabb.min()[axis];
-                //         let end = aabb.max()[axis];
-                //         match cv[axis] {
-                //             0 => start,
-                //             1 => (start + end) / 2.0,
-                //             2 => end,
-                //             _ => unreachable!(),
-                //         }
-                //     })
-                //     .collect();
                 let eval = sdf.evaluate(v);
                 vertices[cv] = eval >= 0.0;
             }
@@ -299,11 +130,8 @@ impl MarchingMesh {
         for tri in mesh2.triangles() {
             let tri_verts = tri
                 .vertices()
-                .map(|(v1, v2)| self.add_vertex(octree.path(), v1, v2));
+                .map(|(v1, v2)| self.add_vertex(octree.path(), v1, v2, sdf));
             let tri2 = MeshTriangle::from(tri_verts);
-            let mut vs = tri2.vertices();
-            vs.sort();
-            assert!(self.triangle_set.insert(vs));
             self.triangles.push(tri2);
         }
     }
@@ -314,19 +142,28 @@ impl MarchingMesh {
         let v = self.aabb.min() + v.mul_elements(self.aabb.dimensions());
         v
     }
-    fn add_vertex(&mut self, path: &OctreePath, mut v1: CubeVertex, mut v2: CubeVertex) -> usize {
+    fn add_vertex(
+        &mut self,
+        path: &OctreePath,
+        mut v1: CubeVertex,
+        mut v2: CubeVertex,
+        sdf: &Sdf,
+    ) -> usize {
         if v2 < v1 {
             mem::swap(&mut v1, &mut v2);
         }
-        let v1 = self.path_position(path, v1);
-        let v2 = self.path_position(path, v2);
-        let v = (v1 + v2) / 2.0;
-        let vnn = v.map(|x| NotNan::new(x).unwrap());
-        *self.vertex_table.entry(vnn).or_insert_with(|| {
-            let index = self.vertices.len();
-            self.vertices.push(v);
-            index
-        })
+        let v1p = self.path_position(path, v1);
+        let v2p = self.path_position(path, v2);
+        let vp = (v1p + v2p) / 2.0;
+        let vnn = vp.map(|x| NotNan::new(x).unwrap());
+        if let Some(v) = self.vertex_table.get(&vnn) {
+            return *v;
+        }
+        let p = self.find_vertex(path, v1, v2, sdf).0;
+        let index = self.vertices.len();
+        self.vertices.push(p);
+        self.vertex_table.insert(vnn, index);
+        index
     }
     fn build_octree(&mut self, tree: &mut Octree<MarchingNode>, sdf: &Sdf) {
         let aabb = tree.path().aabb_inside(&self.aabb);
@@ -353,7 +190,7 @@ impl MarchingMesh {
             .collect::<HashSet<_>>();
         let mut normals = vec![];
         for (v1, v2) in vertices {
-            let (_, normal) = self.find_vertex(&aabb, v1, v2, &sdf);
+            let (_, normal) = self.find_vertex(tree.path(), v1, v2, &sdf);
             normals.push(normal);
         }
         if normals
@@ -376,37 +213,16 @@ impl MarchingMesh {
     }
     fn position_range(&self, aabb: &Aabb, v1: CubeVertex, v2: CubeVertex) -> (Vec3, Vec3) {
         (self.position(aabb, v1), self.position(aabb, v2))
-        // (
-        //     (0..3)
-        //         .map(|axis| match vertex[axis] {
-        //             0 => aabb.min()[axis],
-        //             1 => aabb.min()[axis],
-        //             2 => aabb.max()[axis],
-        //             _ => unreachable!(),
-        //         })
-        //         .collect_array()
-        //         .unwrap()
-        //         .into(),
-        //     (0..3)
-        //         .map(|axis| match vertex[axis] {
-        //             0 => aabb.min()[axis],
-        //             1 => aabb.max()[axis],
-        //             2 => aabb.max()[axis],
-        //             _ => unreachable!(),
-        //         })
-        //         .collect_array()
-        //         .unwrap()
-        //         .into(),
-        // )
     }
     fn find_vertex(
-        &mut self,
-        aabb: &Aabb,
+        &self,
+        path: &OctreePath,
         v1: CubeVertex,
         v2: CubeVertex,
         sdf: &Sdf,
     ) -> (Vec3, Vec3) {
-        let (min, max) = self.position_range(aabb, v1, v2);
+        let min = self.path_position(path, v1);
+        let max = self.path_position(path, v2);
         let range = max - min;
         let lsdf = |t| {
             sdf.evaluate_deriv1(
@@ -420,47 +236,6 @@ impl MarchingMesh {
         let normal: Vec3 = sdf.normal(eval_position);
         (vertex_position, normal)
     }
-    // fn add_vertex(&mut self, aabb: &Aabb, vertex: CubeVertex, sdf: &Sdf) {
-    //     let position_deriv = |t| {
-    //         (0..3)
-    //             .map(|axis| match vertex.into_inner()[axis] {
-    //                 0 => todo!(),
-    //                 1 => todo!(),
-    //                 2 => todo!(),
-    //                 _ => unreachable!(),
-    //             })
-    //             .collect_array()
-    //             .unwrap()
-    //             .into();
-    //         let mut inputs = Vector3::splat(Deriv::nan());
-    //         for axis in 0..3 {
-    //             if ints[axis] % 2 == 0 {
-    //                 inputs[axis] = Deriv::constant(position[axis]);
-    //             } else {
-    //                 inputs[axis] = Deriv::constant(position[axis])
-    //                     + Deriv::variable(t, 0)
-    //                         * Deriv::constant(
-    //                             self.aabb.dimensions()[axis] / (self.render_dim as f64),
-    //                         );
-    //             }
-    //         }
-    //         inputs
-    //     };
-    //     let lsdf = |t| sdf.evaluate_deriv1(position_deriv(t));
-    //     let t = Newton::new().solve(-1.0..1.0, lsdf);
-    //     let t = if let Some(t) = t {
-    //         t.into_inner()
-    //     } else {
-    //         println!("Cannot solve {:?}", ints);
-    //         0.5
-    //     };
-    //     assert!(t >= -1.0 && t <= 1.0, "{:?}", t);
-    //     let t = t.clamp(-0.99, 0.99);
-    //     let position = position_deriv(t).map(|x| x.value());
-    //     let index = self.vertices.len();
-    //     self.vertices.push(position);
-    //     index
-    // }
     fn build_branch(&mut self, tree: &mut Octree<MarchingNode>, sdf: &Sdf) {
         match tree.view_mut() {
             OctreeViewMut::Leaf(_) => {
@@ -507,16 +282,6 @@ impl MarchingMesh {
     ) {
         let aabb = tree.path().aabb_inside(&self.aabb);
         self.add_marching_cube_sub(root, tree, &aabb, sdf);
-        // let mut vertex_ids = vec![];
-        // for vertex in mcube.vertices() {
-        //     let (vertex_position, _) = self.find_vertex(&aabb, *vertex, sdf);
-        //     vertex_ids.push(self.vertices.len());
-        //     self.vertices.push(vertex_position);
-        // }
-        // for tri in mcube.triangles() {
-        //     self.triangles
-        //         .push(MeshTriangle::from(tri.vertices().map(|v| vertex_ids[v])));
-        // }
     }
 
     fn get_neighbors(
